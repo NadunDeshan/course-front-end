0f1e20bbcf4f6d02159fa23c4fe0d87866fca16a
Nadun <nadundeshan16gmail.com>
2026-02-14 13:24:31 +0530

    day 12

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,36 @@ function App() {
 
   return (
     <>
-    <h1>Nadun Deshan</h1>
+    <div className='h-[700px] w-[700px] border-[5px] relative flex justify-center items-center'>
+
+      <div className='h-[100px] w-[300px] bg-yellow-600 flex flex-row justify-center items-center'>
+        <button className='text-white bg-green-800 absolute bottom-0 right-0 p-2'>How can i Help you</button>
+        </div>
+        <div className='w-[300px] h-[300px] bg-pink-400 p-[40px] m-[10px]'>
+          <div className='w-[50px] h-[50px] bg-amber-300 '>
+          </div>
+          <div className='w-[50px] h-[50px] bg-blue-300'>
+          </div>
+
+        </div>
+    </div>
+
+        {/* <div className='h-[100px] w-[100px] bg-green-600'>
+        </div>
+        <div className='h-[100px] w-[100px] absolute bottom-[0px] right-[0px] bg-red-600'>
+        </div>
+        <div className='h-[100px] w-[100px] bg-blue-600'>
+        </div>
+        <div className='h-[100px] w-[100px] fixed right-[10px] bottom-[10px] bg-pink-600'>
+        </div>
+        <div className='h-[100px] w-[100px] bg-gray-600'>
+        </div> */}
+      
+      
+    {/* <h1 className = "text-[#F87C63] text-5xl">Nadun Deshan</h1>
     <ProductCard name="Apple i pad" price="$499" image="https://www.apple.com/assets-www/en_WW/ipad/01_product_tile/large/ipad_air_81133a1a7_2x.jpg"/>
-    <ProductCard name="Mac Book Pro" price="$1299" image="https://www.apple.com/v/macbook-air/x/images/overview/design/color/design_top_skyblue__eepkvlvjzcia_large_2x.jpg"/>
-    
+    <ProductCard name="Mac Book Pro" price="$1299" image="https://www.apple.com/v/macbook-air/x/images/overview/design/color/design_top_skyblue__eepkvlvjzcia_large_2x.jpg"/> */}
+
     </>
   )
 }
